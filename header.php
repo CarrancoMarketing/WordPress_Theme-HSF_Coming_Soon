7764b76c2bf66179db95dedb2870c51812884bfd
lcarranco <lcarranco@gmail.com>
2017-01-07 15:13:17 -0600

    Removed link to Dosis font

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico"> 
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
     <script src="//use.typekit.net/gvd0ojd.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
     <title><?php wp_title();?></title>
