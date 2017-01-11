e12c485b3babd865c93590ca322b63d56540a672
Jose Carranco <carranco.jose.r@gmail.com>
2017-01-11 10:28:05 -0600

    moved closing div element inside loop
    Closing div element for col-md-6 div element was located outside of the
    while loop.

--- front-page.php
+++ front-page.php
@@ -2,12 +2,12 @@
   <div class="container">
       <?php if (have_posts()) : ?>
       <?php while (have_posts()) : the_post(); ?>
-    <div class="col-md-6 col-md-offset-6 content paddingNone">
-      <h2>Website Coming Soon</h2>
-      <p>For deals on Houston's officially licensed sports team merchandise fill out the form below.</p>
-      <?php the_content(); ?>
+        <div class="col-md-6 col-md-offset-6 content paddingNone">
+          <h2>Website Coming Soon</h2>
+          <p>For deals on Houston's officially licensed sports team merchandise fill out the form below.</p>
+          <?php the_content(); ?>
+        </div>
       <?php endwhile; endif;?>
-    </div>
   </div>
 <?php get_footer(); ?> 
 
