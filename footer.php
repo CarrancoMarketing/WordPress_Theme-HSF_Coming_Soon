d92c43aa59e71abf6469e691101e40f2e271a5c6
lcarranco <lcarranco@gmail.com>
2017-01-11 02:58:37 -0600

    Changed some text from h3 to p. Removed text shadow from body. Removed redundant code from stylesheet because it was already included in bootstrap. Changed background color of header and footer to light black. Adjusted height of header and footer background. Adjusted footer copyright to line up with social media icons. Added 30px margin to top of validation error on gravity form. Adjusted width of validation error red line to 100%.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
   <div class="push"></div>
   </div>
   <footer>
-    <div class="container paddingNone">
+    <div class="container">
       <div class="row">
         <div class="col-sm-6 col-xs-12 footercopyright">
           <p>Copyright &copy; <?php echo date(Y); ?> Houston Sports Fan</p>
--- front-page.php
+++ front-page.php
@@ -2,9 +2,9 @@
   <div class="container">
       <?php if (have_posts()) : ?>
       <?php while (have_posts()) : the_post(); ?>
-    <div class="col-md-6 col-md-offset-6">
+    <div class="col-md-6 col-md-offset-6 content paddingNone">
       <h2>Website Coming Soon</h2>
-      <h3>For deals on Houston's officially licensed sports team merchandise fill out the form below.</h3>
+      <p>For deals on Houston's officially licensed sports team merchandise fill out the form below.</p>
       <?php the_content(); ?>
       <?php endwhile; endif;?>
     </div>
