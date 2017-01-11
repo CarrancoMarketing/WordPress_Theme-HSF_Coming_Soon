68bf23b67c263b0a798274701a0158445356f6c4
lcarranco <lcarranco@gmail.com>
2017-01-11 01:20:44 -0600

    Changed social media links to home page links

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
           <p>Copyright &copy; <?php echo date(Y); ?> Houston Sports Fan</p>
         </div>
         <div class="col-sm-6 col-xs-12 socialicons">
-          <a href="https://www.facebook.com/CrossFit-Achieve-1500731070229015/timeline" target="_blank" class="fa fa-facebook-square"></a>
-          <a href="https://www.facebook.com/CrossFit-Achieve-1500731070229015/timeline" target="_blank" class="fa fa-google-plus-square"></a>
+          <a href="https://www.facebook.com/" target="_blank" class="fa fa-facebook-square"></a>
+          <a href="https://plus.google.com/" target="_blank" class="fa fa-google-plus-square"></a>
         </div>
       </div>
     </div>       
